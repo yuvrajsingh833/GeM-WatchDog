6e25a7e7ea6445b209c97a0e48c2d3b803dbb260
Kushagra102 <kushagra.garg77@gmail.com>
2023-09-22 01:50:22 +0530

    Formatted app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,12 +3,11 @@ import ProductInfo from "./components/ProductInfo";
 import { data } from "./components/productData";
 
 function App() {
-  
   return (
     <>
       <h1>Gem vs Amazon</h1>
       <ProductInfo />
-      <div style={{height: 400, width: 800}}>
+      <div style={{ height: 400, width: 800 }}>
         <MyResponsiveLine data={data} />
       </div>
     </>
