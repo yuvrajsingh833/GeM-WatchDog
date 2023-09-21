27e0d37c1edf2e86a3d2598416cb911aa0f9a4f8
Shubham Singh <shubham212a@gmail.com>
2023-09-22 01:39:28 +0530

    added prettier to project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,12 @@
 import ProductInfo from "./components/ProductInfo";
 
 function App() {
-  return <>
-    <h1>Gem vs Amazon</h1>
-    <ProductInfo />
-  </>;
+  return (
+    <>
+      <h1>Gem vs Amazon</h1>
+      <ProductInfo />
+    </>
+  );
 }
 
 export default App;
