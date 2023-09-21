c6258f9d5d40b132be57068a46d6ce7db2285bca
Kushagra102 <kushagra.garg77@gmail.com>
2023-09-22 01:45:18 +0530

    Line Graph Dynamic Analytics Added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,18 @@
+import { MyResponsiveLine } from "./Analytics/MyResponsiveLine";
 import ProductInfo from "./components/ProductInfo";
+import { data } from "./components/productData";
 
 function App() {
-  return <>
-    <h1>Gem vs Amazon</h1>
-    <ProductInfo />
-  </>;
+  
+  return (
+    <>
+      <h1>Gem vs Amazon</h1>
+      <ProductInfo />
+      <div style={{height: 400, width: 800}}>
+        <MyResponsiveLine data={data} />
+      </div>
+    </>
+  );
 }
 
 export default App;
