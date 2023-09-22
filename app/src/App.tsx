30b6fc3ae519b0496c1c5294100299179cfb35f8
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-09-23 01:37:58 +0530

    fe

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen h-fit bg-gray-700">
       <h1 className="text-5xl font-bold text-white mt-10 mb-10">
-        Price Tracker and Visual Analytics
+        GeM Watchdog
       </h1>
       <ProductInfo url={url} />
     </div>
